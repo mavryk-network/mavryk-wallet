09461a855b73316f3a938fc7a79af118f7dbb9af
alexbelz123 <belz.alexx@gmail.com>
2025-01-22 15:07:14 +0200

    multi select sort popup v1

--- src/app/templates/SortPopup/SortPopup.tsx
+++ src/app/templates/SortPopup/SortPopup.tsx
@@ -132,6 +132,107 @@ export const SortPopupContent: FC<SortPopupContentProps> = ({
   );
 };
 
+// Popup content
+export const MultiSortPopupContent: FC<Omit<SortPopupContentProps, 'alternativeLogic'>> = ({
+  items,
+  on,
+  toggle,
+  title = <T id="sortBy" />
+}) => {
+  const { popup } = useAppEnv();
+  const [isLoading, setIslLoading] = useState(false);
+  const [selectedItems, setSelectedItems] = useState<Map<string, SortListItemType>>(() => new Map());
+  const [internalToggleValue, setInternalToggleValue] = useState(on);
+  const { opened, close } = useSortPopup();
+
+  const handleButtonClick = useCallback(async () => {
+    const onClickPromises = Array.from(selectedItems.values()).map(async item => {
+      if (item.onClick) {
+        if (typeof item.onClick === 'function') {
+          const callResult = item.onClick() as unknown as (() => Promise<unknown> | void) | undefined;
+          if (callResult instanceof Promise) {
+            setIslLoading(true);
+            await callResult;
+            setIslLoading(false);
+          }
+        }
+      }
+    });
+
+    // Wait for all onClick handlers to complete
+    await Promise.all(onClickPromises);
+
+    if (internalToggleValue !== on) toggle?.();
+    close();
+  }, [selectedItems, internalToggleValue, on, toggle, close]);
+
+  const handleOptionSelect = useCallback((item: SortListItemType) => {
+    setSelectedItems(prevSelectedItems => {
+      // Create a copy of the Map
+      const updatedItems = new Map(prevSelectedItems);
+
+      if (updatedItems.has(item.id)) {
+        updatedItems.delete(item.id);
+      } else {
+        updatedItems.set(item.id, item);
+      }
+
+      return updatedItems; // Return the updated Map
+    });
+  }, []);
+
+  const handleInternalToggle = useCallback(() => {
+    setInternalToggleValue(!internalToggleValue);
+  }, [internalToggleValue]);
+
+  return (
+    <PopupModalWithTitle
+      isOpen={opened}
+      contentPosition={popup ? 'bottom' : 'center'}
+      onRequestClose={close}
+      title={title}
+      portalClassName="sort-popup"
+    >
+      <div className="flex flex-col mt-2">
+        <ul className={classNames('flex flex-col', popup ? 'px-4' : 'px-12')}>
+          {items.map(item => (
+            <SortListItem
+              key={item.id}
+              item={item}
+              handleOptionSelect={handleOptionSelect}
+              alternativeLogic
+              selectedItemId={selectedItems.get(item.id)?.id}
+            />
+          ))}
+        </ul>
+      </div>
+      {on !== undefined && (
+        <div className={classNames(popup ? 'px-4' : 'px-12')}>
+          <SortDivider />
+          <div className="flex justify-between items-center">
+            <span className="text-sm tracking-normal text-white">
+              <T id="hideZeroBalances" />
+            </span>
+            <Switcher on={on} onClick={handleInternalToggle} />
+          </div>
+        </div>
+      )}
+
+      <div className={classNames('mt-8', popup ? 'px-4' : 'px-12')}>
+        <ButtonRounded
+          size="big"
+          fill
+          onClick={handleButtonClick}
+          className={classNames('w-full')}
+          isLoading={isLoading}
+        >
+          <T id="apply" />
+        </ButtonRounded>
+      </div>
+    </PopupModalWithTitle>
+  );
+};
+
 type SortListItemProps = {
   item: SortListItemType;
   handleOptionSelect: (item: SortListItemType) => void;

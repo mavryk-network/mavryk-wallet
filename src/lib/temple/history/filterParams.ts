542f1b3b6259de3b329bd583df3d46d03e603749
alexbelz123 <belz.alexx@gmail.com>
2025-01-30 14:06:38 +0200

    updated merge system for filters

--- src/lib/temple/history/filterParams.ts
+++ src/lib/temple/history/filterParams.ts
@@ -1,17 +1,6 @@
-import { TzktOperationType } from 'lib/apis/tzkt';
-import { GetOperationsTransactionsParams } from 'lib/apis/tzkt/api';
+import { ExtendedGetOperationsTransactionsParams } from 'lib/apis/tzkt/api';
 
 import { HistoryItemOpTypeEnum } from './types';
-
-export type ExtendedGetOperationsTransactionsParams = Omit<GetOperationsTransactionsParams, 'entrypoint'> & {
-  type: TzktOperationType;
-  hasInternals?: boolean;
-  entrypoint?: string;
-  'entrypoint.null'?: boolean;
-  'parameter.originatedContract.null'?: boolean;
-  'sender.eq'?: string;
-  'target.eq'?: string;
-};
 
 export const createOpParams = (accountAddress: string): StringRecord<ExtendedGetOperationsTransactionsParams> => ({
   [HistoryItemOpTypeEnum.Delegation.toString()]: {
@@ -50,14 +39,48 @@ export const createOpParams = (accountAddress: string): StringRecord<ExtendedGet
   }
 });
 
-export const mergeOpParams = (prevParams: GetOperationsTransactionsParams, params: GetOperationsTransactionsParams) => {
-  const mergedParams: GetOperationsTransactionsParams = { ...prevParams, ...params };
-  // Merge `type` values if both exist
+/**
+ * Used to merge ONLY specofic params of GetOperationsTransactionsParams
+ * @param prevParams createOpParams returned params  object
+ * @param params createOpParams returned params  object
+ * @returns merged params object
+ */
+export const mergeOpParams = (
+  prevParams: ExtendedGetOperationsTransactionsParams,
+  params: ExtendedGetOperationsTransactionsParams
+): ExtendedGetOperationsTransactionsParams => {
+  const mergedParams: ExtendedGetOperationsTransactionsParams = { ...prevParams, ...params };
+
+  // Merge `type` values (OR condition)
   if (prevParams.type && params.type) {
-    // eslint-disable-next-line no-type-assertion/no-type-assertion
-    mergedParams.type = Array.from(new Set(`${prevParams.type},${params.type}`.split(',').map(t => t.trim()))).join(
+    mergedParams.type = Array.from(new Set([...prevParams.type.split(','), ...params.type.split(',')])).join(
       ','
     ) as ExtendedGetOperationsTransactionsParams['type'];
+  }
+
+  // Merge `entrypoint` and `entrypoint.null`
+  const prevEntrypoint = prevParams.entrypoint;
+  const prevEntrypointNull = prevParams['entrypoint.null'];
+  const paramsEntrypoint = params.entrypoint;
+  const paramsEntrypointNull = params['entrypoint.null'];
+
+  if (prevEntrypoint || paramsEntrypoint) {
+    const entrypoints = [prevEntrypoint, paramsEntrypoint].filter(Boolean);
+    mergedParams.entrypoint = entrypoints.length > 1 ? entrypoints.join(' OR ') : entrypoints[0];
+  }
+
+  if (prevEntrypointNull !== undefined || paramsEntrypointNull !== undefined) {
+    mergedParams['entrypoint.null'] =
+      prevEntrypointNull !== undefined && paramsEntrypointNull !== undefined
+        ? prevEntrypointNull || paramsEntrypointNull // OR condition for boolean
+        : prevEntrypointNull !== undefined
+        ? prevEntrypointNull
+        : paramsEntrypointNull;
+  }
+
+  // Merge `hasInternals` using logical OR
+  if (prevParams.hasInternals !== undefined || params.hasInternals !== undefined) {
+    mergedParams.hasInternals = prevParams.hasInternals || params.hasInternals;
   }
 
   return mergedParams;

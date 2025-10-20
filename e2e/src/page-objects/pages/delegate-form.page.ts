fc3dde0fceadea384fed3f46fe45644acfaa9ad4
alexbelz123 <belz.alexx@gmail.com>
2025-10-20 14:47:14 +0300

    updated network fees

--- e2e/src/page-objects/pages/delegate-form.page.ts
+++ e2e/src/page-objects/pages/delegate-form.page.ts
@@ -1,4 +1,4 @@
-import { DelegateFormSelectors } from 'src/app/templates/DelegateForm.selectors';
+import { DelegateFormSelectors } from 'src/app/pages/Stake/delegateForm.selectors';
 
 import { Page } from '../../classes/page.class';
 import { createPageElement } from '../../utils/search.utils';

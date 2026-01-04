a4a721ea82cbec3aa50a9570629cc69e4ae3c996
alexbelz123 <belz.alexx@gmail.com>
2026-01-04 22:34:16 +0200

    edit account name popup

--- src/app/pages/EditAccount/EditAccount.tsx
+++ src/app/pages/EditAccount/EditAccount.tsx
@@ -1,45 +1,43 @@
-import React, { FC, FormEventHandler, useCallback, useEffect, useMemo, useRef } from 'react';
+import React, { FC, useCallback, useMemo, useState } from 'react';
 
 import clsx from 'clsx';
 
-import { FormField, HashChip } from 'app/atoms';
-import { ACCOUNT_NAME_PATTERN } from 'app/defaults';
 import { useAppEnv } from 'app/env';
+import { ReactComponent as KeyIcon } from 'app/icons/key.svg';
 import { ReactComponent as TrashIcon } from 'app/icons/trash.svg';
 import PageLayout from 'app/layouts/PageLayout';
-import { BTN_ERROR, ButtonRounded } from 'app/molecules/ButtonRounded';
-import { useFormAnalytics } from 'lib/analytics';
-import { T, t } from 'lib/i18n';
-import {
-  useAccount,
-  useContactsActions,
-  useFilteredContacts,
-  useSetAccountPkh,
-  useTempleClient
-} from 'lib/temple/front';
-import { useAlert } from 'lib/ui';
+import { BTN_ERROR } from 'app/molecules/ButtonRounded';
+import { ListItemWithNavigate, ListItemWithNavigateprops } from 'app/molecules/ListItemWithNavigate';
+import AccountBanner from 'app/templates/AccountBanner';
+import { t } from 'lib/i18n';
+import { useAccount, useContactsActions, useFilteredContacts, useSetAccountPkh } from 'lib/temple/front';
 import { useConfirm } from 'lib/ui/dialog';
 import { goBack, navigate } from 'lib/woozie';
 
-import { EditableTitleSelectors } from './editAccount.selectors';
+import { EditAccountNamePopup } from './popups/EditAccountNamePopup';
 
 export type EditAccountProps = {
   accHash?: string | null;
 };
 
 export const EditAccount: FC<EditAccountProps> = ({ accHash }) => {
-  const { editAccountName } = useTempleClient();
   const setAccountPkh = useSetAccountPkh();
   const { allContacts: filteredContacts } = useFilteredContacts();
-  const { removeContact, editContact } = useContactsActions();
+  const { removeContact } = useContactsActions();
   const account = useAccount();
-  const customAlert = useAlert();
-  const formAnalytics = useFormAnalytics('ChangeAccountName');
+
   const confirm = useConfirm();
   const { popup } = useAppEnv();
 
-  const editAccNameFieldRef = useRef<HTMLInputElement>(null);
-  const accNamePrevRef = useRef<string>();
+  const [showEditNamePopup, setShowEditNamePopup] = useState(false);
+
+  const handleEditAccountOpen = useCallback(() => {
+    setShowEditNamePopup(true);
+  }, []);
+
+  const handleEditAccountClose = useCallback(() => {
+    setShowEditNamePopup(false);
+  }, []);
 
   const accToChange = filteredContacts.find(acc => acc.address === accHash);
 
@@ -47,57 +45,9 @@ export const EditAccount: FC<EditAccountProps> = ({ accHash }) => {
     () => (accToChange ? accToChange.address : account.publicKeyHash),
     [accToChange, account.publicKeyHash]
   );
-  const accountName = useMemo(() => (accToChange ? accToChange.name : account.name), [accToChange, account.name]);
   const isOwn = useMemo(
     () => (accToChange?.accountInWallet ? accToChange.accountInWallet : false),
     [accToChange?.accountInWallet]
-  );
-
-  useEffect(() => {
-    accNamePrevRef.current = accountName;
-  }, [accountName]);
-
-  const autoCancelTimeoutRef = useRef<number>();
-
-  useEffect(
-    () => () => {
-      clearTimeout(autoCancelTimeoutRef.current);
-    },
-    []
-  );
-
-  const handleEditSubmit = useCallback<FormEventHandler>(
-    evt => {
-      evt.preventDefault();
-
-      (async () => {
-        formAnalytics.trackSubmit();
-        try {
-          const newName = editAccNameFieldRef.current?.value;
-          if (newName && newName !== accountName && isOwn) {
-            // update "own" account name
-            await editAccountName(accountHash, newName);
-          } else if (!isOwn && newName && newName !== accountName) {
-            // update contact from address book
-            await editContact(accountHash, { name: newName });
-          }
-
-          formAnalytics.trackSubmitSuccess();
-
-          goBack();
-        } catch (err: any) {
-          formAnalytics.trackSubmitFail();
-
-          console.error(err);
-
-          await customAlert({
-            title: t('errorChangingAccountName'),
-            children: err.message
-          });
-        }
-      })();
-    },
-    [formAnalytics, accountName, isOwn, editAccountName, accountHash, editContact, customAlert]
   );
 
   const handleRemoveContactClick = useCallback(async () => {
@@ -123,9 +73,24 @@ export const EditAccount: FC<EditAccountProps> = ({ accHash }) => {
     goBack();
   }, [accountHash, confirm, isOwn, removeContact, setAccountPkh]);
 
-  const handleEditFieldFocus = useCallback(() => {
-    clearTimeout(autoCancelTimeoutRef.current);
-  }, []);
+  // revealPrivateKey
+  const accountOptions: ListItemWithNavigateprops[] = useMemo(
+    () => [
+      {
+        i18nKey: 'editName',
+        Icon: KeyIcon,
+        onClick: handleEditAccountOpen,
+        fillIcon: false
+      },
+      {
+        linkTo: '/settings/reveal-private-key',
+        i18nKey: 'revealPrivateKey',
+        Icon: KeyIcon,
+        fillIcon: false
+      }
+    ],
+    [handleEditAccountOpen]
+  );
 
   return (
     <PageLayout
@@ -144,30 +109,23 @@ export const EditAccount: FC<EditAccountProps> = ({ accHash }) => {
         )}
       >
         <div className="flex flex-col gap-1 mb-4">
-          <div className="text-primary-white text-base-plus">
-            <T id="publicAddress" />:
-          </div>
-          <HashChip hash={accountHash} small trim={false} />
+          <AccountBanner account={account} restrictAccountSelect />
         </div>
-        <form className="flex flex-col items-center flex-1 justify-start gap-3" onSubmit={handleEditSubmit}>
-          <FormField
-            ref={editAccNameFieldRef}
-            name="name"
-            defaultValue={accountName}
-            maxLength={16}
-            label={isOwn ? t('editAccountName') : t('editContactName')}
-            placeholder={t('enterAccountName')}
-            pattern={ACCOUNT_NAME_PATTERN.toString().slice(1, -1)}
-            title={t('accountNameInputTitle')}
-            spellCheck={false}
-            onFocus={handleEditFieldFocus}
-          />
 
-          <ButtonRounded size="big" className="w-full capitalize mt-auto" testID={EditableTitleSelectors.saveButton}>
-            <T id="save" />
-          </ButtonRounded>
-        </form>
+        <ul className={clsx('flex flex-col pb-4')}>
+          {accountOptions.map(({ ...option }) => (
+            <ListItemWithNavigate key={option.i18nKey} {...option} />
+          ))}
+        </ul>
       </div>
+      <EditAccountNamePopup
+        opened={showEditNamePopup}
+        close={handleEditAccountClose}
+        name={account.name}
+        accountHash={account.publicKeyHash}
+        isOwn={isOwn}
+        accToChange={accToChange}
+      />
     </PageLayout>
   );
 };
